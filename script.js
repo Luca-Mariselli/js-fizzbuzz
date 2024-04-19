58ca9b1c1e61dec25acd3074829aee45ee2363aa
Luca-Mariselli <lucamariselli@gmail.com>
2024-04-19 15:45:39 +0200

    tentativo stampa in pagina

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Stampo numeri da 1 a 100
 for (let myNum = 1; myNum <= 100; myNum++) {
+    const newDiv = document.querySelector(`div`);
     // Sia multipli di 3 che di 5
     if(myNum % 3 == 0 && myNum % 5 == 0){
         console.log(`FizzBuzz`)
+        newDiv.innerHTML = newDiv.innerHTML + `<div>FizzBuzz</div>`    
     }
     // Multipli solo di 3
     else if(myNum % 3 == 0){
         console.log(`Fizz`)
+        newDiv.innerHTML = newDiv.innerHTML + `<div>Fizz</div>`    
     }
     // Multipli solo di 5
     else if(myNum % 5 == 0){
         console.log(`Buzz`)
+        newDiv.innerHTML = newDiv.innerHTML + `<div>Buzz</div>`
     }
-    else{console.log(myNum)}
-    
+    else{console.log(myNum)};
+    let myNewNum = myNum;    
+    newDiv.innerHTML = newDiv.innerHTML + `<div>${myNum}</div>`
 }
